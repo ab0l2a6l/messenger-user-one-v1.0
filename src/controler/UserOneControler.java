9d4bb467d33ead47dcad0dcd3f70cd4087eaa2be
abolfazl <a.b.f.m.98@gmail.com>
2023-06-09 13:57:22 +0430

    add controler

--- src/controler/UserOneControler.java
+++ src/controler/UserOneControler.java
@@ -1,4 +1,21 @@
 package controler;
 
+import model.entity.UserOne;
+import model.service.UserOneService;
+import model.service.UserOneServiceRead;
+import model.service.UserOneServiceWrite;
+
+import java.util.List;
+
 public class UserOneControler {
+    UserOneServiceWrite write = new UserOneService();
+    UserOneServiceRead read = new UserOneService();
+
+    public void save(UserOne userOne) {
+        write.save(userOne);
+    }
+
+    public List<UserOne> findByAll(long id) {
+        return read.findByALl(id);
+    }
 }
