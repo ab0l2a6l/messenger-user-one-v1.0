a7574d0920c216e6324aa2a29f45d6f03bd57731
abolfazl <a.b.f.m.98@gmail.com>
2023-06-09 13:08:47 +0430

    tolid class ha va sakht scanner

---
+++ src/common/StaticScanner.java
@@ -0,0 +1,9 @@
+package common;
+
+import java.util.Scanner;
+
+public class StaticScanner {
+    public static Scanner getDate() {
+        return new Scanner(System.in);
+    }
+}
---
+++ src/view/Main.java
@@ -0,0 +1,9 @@
+package view;
+
+import common.StaticScanner;
+
+import java.util.Scanner;
+
+public class Main {
+    Scanner input = StaticScanner.getDate();
+}
